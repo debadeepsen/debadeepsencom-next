75328577f6eb4827756adeadb41949818ec4fbfc
Debadeep Sen <dsen@msi.home>
2023-07-30 03:43:28 +0530

    window fix

--- src/lib/commonLib.ts
+++ src/lib/commonLib.ts
@@ -5,12 +5,12 @@ import { LOCAL_STORAGE_KEYS } from './constants/commonConstants'
 
 export const colorModeUtil = {
   get manualPreferenceSet() {
-    return !!window.localStorage.getItem(LOCAL_STORAGE_KEYS.COLOR_MODE)
+    return !!window?.localStorage.getItem(LOCAL_STORAGE_KEYS.COLOR_MODE)
   },
   get value() {
-    return window.localStorage.getItem(LOCAL_STORAGE_KEYS.COLOR_MODE) as ColorModeValueType
+    return window?.localStorage.getItem(LOCAL_STORAGE_KEYS.COLOR_MODE) as ColorModeValueType
   },
   set: (value: string) => {
-    window.localStorage.setItem(LOCAL_STORAGE_KEYS.COLOR_MODE, value)
+    window?.localStorage.setItem(LOCAL_STORAGE_KEYS.COLOR_MODE, value)
   }
 }
