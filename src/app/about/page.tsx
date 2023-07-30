135a355222f53eb235f17a9b4ae8aebc7ab77ac8
Debadeep Sen <dsen@msi.home>
2023-07-30 17:53:19 +0530

    blog page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,20 +1,22 @@
 /* eslint-disable @next/next/no-img-element */
+import Divider from '@/components/Divider'
+import Socials from '@/components/Socials'
 import { H1 } from '@/components/Title'
 import React from 'react'
 
 const About = () => {
   return (
-    <div className='w-[600px] lg:w-[800px] mx-auto'>
+    <div className='w-full md:w-[600px] lg:w-[800px] mx-auto'>
       <div className='text-center'>
         <H1>About Me</H1>
       </div>
       <div className='flex flex-col lg:flex-row'>
-        <div>
+        <div className='flex justify-center'>
           <img
             src='https://scontent.fccu20-1.fna.fbcdn.net/v/t39.30808-6/318581062_10160012150610169_5609406085622197679_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=5icC1EnZEegAX-SBvqd&_nc_ht=scontent.fccu20-1.fna&oh=00_AfBHuStyZ-lDxNTdkhlYyJUTuOdyYcjdjqn5t6bgrGIAYQ&oe=64CACA47'
             alt='profile picture'
-            className='w-[360px] h-[360px] object-cover rounded-full shadow-xl'
-            style={{filter: 'sepia(.3) saturate(.8) contrast(1.2)'}}
+            className='w-[180px] h-[180px] md:w-[360px] md:h-[360px] object-cover rounded-full shadow-xl'
+            style={{ filter: 'sepia(.3) saturate(.8) contrast(1.2)' }}
           />
         </div>
         <div className='mt-8 lg:mt-0 lg:ml-16'>
@@ -32,7 +34,18 @@ const About = () => {
             photographer in the past. I&apos;m also an ardent Marvel/MCU nerd
             (this website is a spoiler-free zone, though).
           </p>
+          <p>
+            I&apos;ve worked with a variety of tech stacks, but recently
+            I&apos;ve been more busy with making front-end applications,
+            primarily using either Vue or React. Both libraries have their
+            merit, but I&apos;ve been fascinated with Next.js lately, so React
+            is my preferred choice of front-end framework right now.
+          </p>
         </div>
+      </div>
+      <div>
+        <Divider />
+        <Socials />
       </div>
     </div>
   )
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,8 +1,20 @@
+import { H1 } from '@/components/Title'
 import React from 'react'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='w-full lg:w-[800px] xl:w-[1024px] mx-auto'>
+      <H1>Blog</H1>
+      <p>
+        I mostly write on{' '}
+        <a target='_blank' href='https://dev.to/debadeepsen'>
+          Dev.to
+        </a>,{' '}
+        but now that this website is ready, I might switch over or have a
+        healthy mix. I&apos;m planning on setting up a newsletter some time
+        soon, but until then, keep an eye on this page for new articles.
+      </p>
+    </div>
   )
 }
 
