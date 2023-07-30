ecd926e3e3c79158db2824350ef3b860791d541f
Debadeep Sen <dsen@msi.home>
2023-07-30 15:50:41 +0530

    refactor

--- src/components/LoadingOverlay.tsx
+++ src/components/LoadingOverlay.tsx
@@ -1,47 +1,9 @@
-import { THEME_COLOR } from '@/lib/constants/commonConstants'
-import React from 'react'
+import LoadingSVG from "./SVGs/LoadingSVG"
 
 const LoadingOverlay = () => {
   return (
-    <div className='fixed z-20 left-0 top-0 w-[100vw] h-[100vh] border-box bg-white flex justify-center items-center'>
-      {/* Thanks to https://codepen.io/nikhil8krishnan/pen/rVoXJa for this SVG! */}
-      <svg
-        version='1.1'
-        id='L4'
-        xmlns='http://www.w3.org/2000/svg'
-        width={100}
-        height={100}
-        viewBox='0 0 100 100'
-        enable-background='new 0 0 0 0'
-      >
-        <circle fill={THEME_COLOR} stroke='none' cx='6' cy='50' r='6'>
-          <animate
-            attributeName='opacity'
-            dur='1s'
-            values='0;1;0'
-            repeatCount='indefinite'
-            begin='0.1'
-          ></animate>
-        </circle>
-        <circle fill={THEME_COLOR} stroke='none' cx='26' cy='50' r='6'>
-          <animate
-            attributeName='opacity'
-            dur='1s'
-            values='0;1;0'
-            repeatCount='indefinite'
-            begin='0.2'
-          ></animate>
-        </circle>
-        <circle fill={THEME_COLOR} stroke='none' cx='46' cy='50' r='6'>
-          <animate
-            attributeName='opacity'
-            dur='1s'
-            values='0;1;0'
-            repeatCount='indefinite'
-            begin='0.3'
-          ></animate>
-        </circle>
-      </svg>
+    <div className='fixed z-20 left-0 top-0 w-[100vw] h-[100vh] border-box bg-gray-200 flex justify-center items-center'>
+      <LoadingSVG />
     </div>
   )
 }
---
+++ src/components/SVGs/LoadingSVG.tsx
@@ -0,0 +1,49 @@
+import { THEME_COLOR } from '@/lib/constants/commonConstants'
+import React from 'react'
+
+const LoadingSVG = () => {
+  {
+    /* Thanks to https://codepen.io/nikhil8krishnan/pen/rVoXJa for this SVG! */
+  }
+  return (
+    <svg
+      version='1.1'
+      id='L4'
+      xmlns='http://www.w3.org/2000/svg'
+      width={100}
+      height={100}
+      viewBox='0 0 100 100'
+      enable-background='new 0 0 0 0'
+    >
+      <circle fill={THEME_COLOR} stroke='none' cx='6' cy='50' r='6'>
+        <animate
+          attributeName='opacity'
+          dur='1s'
+          values='0;1;0'
+          repeatCount='indefinite'
+          begin='0.1'
+        ></animate>
+      </circle>
+      <circle fill={THEME_COLOR} stroke='none' cx='26' cy='50' r='6'>
+        <animate
+          attributeName='opacity'
+          dur='1s'
+          values='0;1;0'
+          repeatCount='indefinite'
+          begin='0.2'
+        ></animate>
+      </circle>
+      <circle fill={THEME_COLOR} stroke='none' cx='46' cy='50' r='6'>
+        <animate
+          attributeName='opacity'
+          dur='1s'
+          values='0;1;0'
+          repeatCount='indefinite'
+          begin='0.3'
+        ></animate>
+      </circle>
+    </svg>
+  )
+}
+
+export default LoadingSVG
