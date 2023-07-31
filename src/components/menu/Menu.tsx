5867f511e3081faef8ca5d23f6f49f8bbb9abc7f
Debadeep Sen <dsen@msi.home>
2023-07-31 15:19:35 +0530

    adding responsiveness

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -3,18 +3,17 @@ import { THEME_COLOR } from '@/lib/constants/commonConstants'
 import { RootState } from '@/store'
 import { useAppSelector } from '@/store/hooks'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 
 const MenuItem = ({ href, text }: { href: string; text: string }) => {
   const colorModeValue = useAppSelector(
     (state: RootState) => state.colorMode.value
   )
-
   return (
     <Link
       href={href}
       className={
-        'text-[16px] flex justify-center items-center text-white transition-all rounded-md m-1 px-4 py-2 w-[80px] bg-white/20 hover:bg-white/30 hover:no-underline' +
+        'text-[16px] flex justify-center items-center text-white transition-all rounded-xs md:rounded-md m-1 px-4 py-2 w-[80px] bg-white/20 hover:bg-white/40 hover:no-underline' +
         (colorModeValue === 'light' ? ' bg-white/50 hover:bg-white/80' : '')
       }
       style={{ color: ColorModes[colorModeValue].menu }}
@@ -24,12 +23,48 @@ const MenuItem = ({ href, text }: { href: string; text: string }) => {
   )
 }
 
+const MenuList = () => (
+  <>
+    <MenuItem href='/' text='Home' />
+    <MenuItem href='/about' text='About' />
+    <MenuItem href='/blog' text='Blog' />
+  </>
+)
+
 const Menu = () => {
+  const colorModeValue = useAppSelector(
+    (state: RootState) => state.colorMode.value
+  )
+
+  const [menuOpacity, setMenuOpacity] = useState(0)
+  const toggleMenu = () => {
+    const newValue = menuOpacity === 0 ? 1 : 0
+    setMenuOpacity(newValue)
+  }
+
   return (
-    <div className='flex fixed top-[20px] right-[20px] z-20'>
-      <MenuItem href='/' text='Home' />
-      <MenuItem href='/about' text='About' />
-      <MenuItem href='/blog' text='Blog' />
+    <div className='fixed top-[20px] right-[20px] z-20'>
+      <div className='hidden md:flex'>
+        <MenuList />
+      </div>
+      <div className='md:hidden relative'>
+        <button
+          className='bg-transparent border-0'
+          onClick={() => toggleMenu()}
+        >
+          <i
+            className='fas fa-bars'
+            style={{ fontSize: 20, color: ColorModes[colorModeValue].menu }}
+          ></i>
+        </button>
+        <div
+          className={
+            'absolute top-[24px] right-0 transition-all opacity-' + menuOpacity
+          }
+        >
+          <MenuList />
+        </div>
+      </div>
     </div>
   )
 }
