09faf344e661d4ec6686092df3dce31da57f023e
Debadeep Sen <dsen@msi.home>
2023-08-03 22:07:54 +0530

    attributions

---
+++ src/data/attributions.ts
@@ -0,0 +1,14 @@
+export type AttributionsType = {
+  name: string
+  source: string
+}
+
+const attributions: AttributionsType[] = [
+  {
+    name: 'favicon',
+    source: 'https://icons8.com/icon/48250/code'
+  },
+  { name: 'SVGs', source: 'https://undraw.com' }
+]
+
+export default attributions
