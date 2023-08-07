19fa03c5d7c45ed6b1879606652ba959e98a9e2a
debadeepsensl <124399771+debadeepsensl@users.noreply.github.com>
2023-08-07 14:59:20 +0530

    mobile fix

--- src/components/containers/PageSection.tsx
+++ src/components/containers/PageSection.tsx
@@ -8,7 +8,7 @@ import { PageSectionType } from '@/types/types'
 import React from 'react'
 
 const h2Class =
-  'text-xl sm:text-3xl text-white px-6 inline-block text-[1.2rem] font-extralight relative shadow-[0px_2px_3px_#0004] -ml-10 mb-[30px] pl-5 p-2.5 before:content-[""] before:block before:absolute before:w-0 before:h-0 before:border-l-[none] before:border-l-[#0000] before:border-r-[#255] before:border-t-[#255] before:border-b-[#0000] before:border-solid before:border-[7px] before:left-0 before:-bottom-3.5 left-[-6px] top-[-10px]'
+  'text-xl sm:text-3xl text-white px-6 inline-block text-[1.2rem] font-extralight relative shadow-[0px_2px_3px_#0004] -ml-6 sm:-ml-10 mb-[30px] pl-5 p-2.5 before:content-[""] before:block before:absolute before:w-0 before:h-0 before:border-l-[none] before:border-l-[#0000] before:border-r-[#255] before:border-t-[#255] before:border-b-[#0000] before:border-solid before:border-[7px] before:left-0 before:-bottom-3.5 left-[-6px] top-[-10px]'
 
 const divClasses =
   'relative pt-10 flex w-full min-[1280px]:w-[480px] max-[1280px]:justify-center'
