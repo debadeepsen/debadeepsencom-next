0f49fa59062f382c318d88298fb45d15096dc81e
Debadeep Sen <dsen@msi.home>
2023-08-15 22:39:16 +0530

    preload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,12 @@
 'use client'
 
 import './styles/globals.css'
-import Script from 'next/script'
 import { ReactNode } from 'react'
 import { Provider } from 'react-redux'
 import { store } from '@/store'
 import Main from '@/components/containers/Main'
-import { useAppDispatch } from '@/store/hooks'
-import { setOpen } from '@/store/slices/menuSlice'
+import ThirdPartyScripts from '@/components/ThirdPartyScripts'
+import { photosDir, photosToShow } from '@/lib/constants/photosContants'
 
 const metadata = {
   title: 'Debadeep Sen',
@@ -18,32 +17,7 @@ const metadata = {
 const Html = ({ children }: { children: ReactNode }) => {
   return (
     <html lang='en'>
-      <Script
-        src='https://kit.fontawesome.com/43640773c8.js'
-        crossOrigin='anonymous'
-        defer
-      />
-      <Script defer src='https://code.iconify.design/1/1.0.7/iconify.min.js' />
-      <Script
-        async
-        src='https://www.googletagmanager.com/gtag/js?id=G-H7GJ1WQ6ZP'
-      ></Script>
-      <Script id='google-analytics'>
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-        
-          gtag('config', 'G-H7GJ1WQ6ZP');
-        `}
-      </Script>
-      <Script id='gtm'>
-        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-MHPK7PX4');`}
-      </Script>
+      <ThirdPartyScripts />
       <head>
         <meta name='title' content={metadata.title}></meta>
         <meta name='description' content={metadata.description}></meta>
@@ -53,6 +27,14 @@ const Html = ({ children }: { children: ReactNode }) => {
         />
         <meta property='og:image:width' content='200' />
         <meta property='og:image:height' content='200' />
+        {photosToShow.map(file => (
+          <link
+            key={file.filename}
+            rel='preload'
+            href={photosDir + file.filename}
+            type='image/webp'
+          />
+        ))}
         <title>{metadata.title}</title>
       </head>
       <body>
