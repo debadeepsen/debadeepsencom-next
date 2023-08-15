97f2e5e3b4f63ca933ea984287f5f7414769a996
Debadeep Sen <dsen@msi.home>
2023-08-15 23:38:25 +0530

    photogrid wrapper

--- src/app/photography/page.tsx
+++ src/app/photography/page.tsx
@@ -1,15 +1,4 @@
-'use client'
-
-import PhotoGrid from '@/components/PhotoGrid'
-/* eslint-disable @next/next/no-img-element */
-import { H1 } from '@/components/Title'
-import Card from '@/components/containers/Card'
-import { ColorModes } from '@/lib/constants/colorModeConstants'
-import { photosDir, photosToShow } from '@/lib/constants/photosContants'
-import { RootState } from '@/store'
-import { useAppSelector } from '@/store/hooks'
-import Image from 'next/image'
-import { useState } from 'react'
+import PhotoGridWrapper from '@/components/containers/PhotoGridWrapper'
 
 export const metadata = {
   title: 'Photography | Debadeep Sen',
@@ -20,7 +9,7 @@ export const metadata = {
 
 const Photography = () => (
   <div>
-    <PhotoGrid />
+    <PhotoGridWrapper />
   </div>
 )
 
---
+++ src/components/containers/PhotoGridWrapper.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import React from 'react'
+import PhotoGrid from '../PhotoGrid'
+
+const PhotoGridWrapper = () => {
+  return <PhotoGrid />
+}
+
+export default PhotoGridWrapper
