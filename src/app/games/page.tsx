f5bf090304a5651f89ad92803d8e568062e388ac
debadeepsensl <124399771+debadeepsensl@users.noreply.github.com>
2023-08-16 21:19:26 +0530

    games

---
+++ src/app/games/page.tsx
@@ -0,0 +1,14 @@
+import { H1 } from '@/components/Title'
+import React from 'react'
+
+const Games = () => {
+  return (
+    <div className='w-full lg:w-[800px] xl:w-[1024px] min-[2000px]:w-[80%] mx-auto'>
+      <H1>Games</H1>
+      Enjoy a few cool games written by me (with help from
+      JavaScript/TypeScript, and associated libraries)
+    </div>
+  )
+}
+
+export default Games
