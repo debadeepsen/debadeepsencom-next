9dc469c9bf821109be6153b2451a6a9e9c6765b4
debadeepsensl <124399771+debadeepsensl@users.noreply.github.com>
2023-08-16 23:14:37 +0530

    game constants

--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -1,12 +1,14 @@
 import { H1 } from '@/components/Title'
-import React from 'react'
+import React, { ReactNode } from 'react'
 
 const Games = () => {
   return (
     <div className='w-full lg:w-[800px] xl:w-[1024px] min-[2000px]:w-[80%] mx-auto'>
       <H1>Games</H1>
-      Enjoy a few cool games written by me (with help from
-      JavaScript/TypeScript, and associated libraries)
+      <p>
+        Enjoy a few cool games written by me (with help from
+        JavaScript/TypeScript, and associated libraries)
+      </p>
     </div>
   )
 }
---
+++ src/lib/constants/gameConstants.ts
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react'
+
+export type GameInfo = {
+  icon?: ReactNode
+  title?: string
+  description?: string
+}
+
+export const gameList: GameInfo[] = [
+  {
+    icon: undefined
+  }
+]
